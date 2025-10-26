d6aaca9fe49dc187a0904916231dda1bb2674afa
MITHRA--SILIVERU <115676124+MITHRA-SILIVERU@users.noreply.github.com>
2025-10-26 21:09:36 +0530

    Change email transporter to use Gmail service
    Updated email transporter configuration to use Gmail service.

--- src/server/server.js
+++ src/server/server.js
@@ -22,16 +22,12 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 let contactEmail;
 if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
 const contactEmail = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT), // 465
-  secure: true, // true for port 465, false for 587
+  service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, // Use Gmail App Password
     },
-   tls: {
-    rejectUnauthorized: false // Helps with some hosting providers
-  }
+ 
   });
 
   // Verify email configuration
